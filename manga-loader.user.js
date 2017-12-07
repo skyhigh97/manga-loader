02f4f325baf4938b34ed3d97eee082e528596aac
Akash Dash <akash.dash.vicky@gmail.com>
2017-12-07 14:59:49 +0530

    changed mangafox.me to mangafox.la

--- manga-loader.user.js
+++ manga-loader.user.js
@@ -9,7 +9,7 @@
 // @grant GM_setValue
 // @grant GM_deleteValue
 // @match *://bato.to/reader*
-// @match *://mangafox.me/manga/*/*/*
+// @match *://mangafox.la/manga/*/*/*
 // @match *://readms.net/r/*/*
 // @match *://mangastream.com/r/*/*/*/*
 // @match *://mangastream.com/read/*/*/*/*
@@ -214,7 +214,7 @@ var implementations = [{
   prevchap: '#mangainfofooter > #mangainfo_bas table tr:last-child a'
 }, {
   name: 'mangafox',
-  match: "^https?://mangafox.me/manga/[^/]*/[^/]*/[^/]*",
+  match: "^https?://mangafox.la/manga/[^/]*/[^/]*/[^/]*",
   img: '.read_img img',
   next: '.read_img a',
   numpages: function() {
